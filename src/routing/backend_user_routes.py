05fb740988a1ee9dc87e50d0b411a5dde283d38b
DavidCastillo2 <58519904+DavidCastillo2@users.noreply.github.com>
2021-11-12 17:53:32 -0500

    Added some parameters. 1 TODO created

--- src/routing/backend_user_routes.py
+++ src/routing/backend_user_routes.py
@@ -5,46 +5,96 @@ backend_user_app = Blueprint("backend_user_routes", __name__, url_prefix="/backe
 
 @backend_user_app.route('/check_login')
 def check_login():
+    """
+    Checks if a Username and Password is correct
+
+    :param Username: Optional "kind" of ingredients.
+    :type Username: str
+    :param Password: Optional "kind" of ingredients.
+    :type Password: str
+
+    :return: CSV[ProductID, WasOnSaleBoolean, TimePurchased, TimeAddedToList, CountOfProduct]
+    :rtype: list[str]
+    """
     return None
 
 
 @backend_user_app.route('/add_user')
 def add_user():
+    """
+    Adds a User to [User] Table
+
+    :param Username_Hash: Optional "kind" of ingredients.
+    :type Username_Hash: str
+    :param Password_Hash: Optional "kind" of ingredients.
+    :type Password_Hash: str
+
+    :return: Boolean for Succeeded or Failed
+    :rtype: Boolean
+    """
     return None
 
 
 @backend_user_app.route('/add_transactions')
 def add_transactions():
+    """
+    Add a Transaction Entry to [Transactions] Table
+
+    :param Product_ID: Product Key
+    :type Product_ID: int
+    :param Price_History_ID: Price History Key
+    :type Price_History_ID: int
+    :param Username_Hash: Hashed version of a Username
+    :type Username_Hash: str
+    :param Time_Added: Datetime for time this item was added
+    :type Time_Added: str
+
+    :return: Boolean for Succeeded or Failed
+    :rtype: Boolean
+    """
     return None
 
 
+# TODO
+# Why does this exist? Why is a User getting a history of something
 @backend_user_app.route('/get_transactions')
 def get_transactions():
     return None
 
 
 @backend_user_app.route('/add_alert')
-def login():
+def add_alert():
+    """
+    Adds a Product_ID to [User_Alert_List] table
+
+    :param Username_Hash: Optional "kind" of ingredients.
+    :type Username_Hash: str
+    :param Password: Optional "kind" of ingredients.
+    :type Password: str
+
+    :return: Boolean for Succeeded or Failed
+    :rtype: Boolean
+    """
     return None
 
 
 @backend_user_app.route('/get_alerts')
-def login():
+def get_alerts():
     return None
 
 
 @backend_user_app.route('/remove_alerts')
-def login():
+def remove_alerts():
     return None
 
 
 @backend_user_app.route('/add_shopping_list')
-def login():
+def add_shopping_list():
     return None
 
 
 @backend_user_app.route('/get_shopping_list')
-def login():
+def get_shopping_list():
     return None
 
 
